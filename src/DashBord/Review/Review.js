431416575469e69213c7057a7b86274dc86e4044
sayamkhanraj <snsayamkhanraj@gmail.com>
2021-11-13 14:00:27 +0600

    add review star

--- src/DashBord/Review/Review.js
+++ src/DashBord/Review/Review.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import './Review.css'
 
 const Review = () => {
+         const { register, handleSubmit, watch, reset, formState: { errors } } = useForm();
+
+         const onSubmit = data => {
+                  console.log(data)
+                  fetch('http://localhost:5000/reviews',{
+                           method:"POST",
+                           headers:{'content-Type' : 'application/json'},
+                           body: JSON.stringify(data)
+                  })
+                  .then(res => res.json())
+                  .then(result => console.log(result))
+
+
+                  reset();
+         };
+
+         console.log(watch("example")); 
          return (
-                  <div>
-                           <h1>This is Review page</h1>
+                  <div className="d-flex justify-content-center">
+                        <div className="w-75">
+                  <form className="add-form" onSubmit={handleSubmit(onSubmit)}>
+                           <h3 className="mt-2 text-white">Add Review</h3>
+                  <input placeholder="Your Name" {...register("name")} />
+                  <input placeholder="Leave Stars (1 - 5)" {...register("rating", { required: true })} />
+                  <input placeholder="Discription" {...register("discription", { required: true })} />
+                  {errors.exampleRequired && <span>This field is required</span>}
+                  
+                  <input className="fw-bolder fs-5" type="submit" />
+                  </form>
+                  </div>    
                   </div>
          );
 };
--- src/Pages/Home/DisplayReview/DisplayReview.js
+++ src/Pages/Home/DisplayReview/DisplayReview.js
@@ -14,14 +14,13 @@ const DisplayReview = () => {
                   <div>
                   <div className="container my-5">
                   <div className="proucts-text">
-                  <h2 className="text-center fw-bold products-text mb-3">Reviews</h2>
+                  <h2 className="text-center fw-bold products-text mb-5">Reviews</h2>
                   </div>
                   <div className="row">
                   {
                            reviews.map(review => 
-                                    <div class="col-md-4">
-                                    <div class="col">
-                                    <div class="card h-100">
+                                    <div class="col-md-4 mb-3">
+                                    <div class="card h-100 card-style">
                                     <div class="card-body">
                                     <h3 class="card-title header-text text-center mb-3">{review?.name}</h3>
                                     <div className="d-flex justify-content-center align-items-center"><Rating
@@ -33,7 +32,7 @@ const DisplayReview = () => {
                                     <p class="card-tex mt-3">{review?.discription}</p>
                                     </div>
                                     </div>
-                                    </div>
+                              
                                    </div>
 
                             )
